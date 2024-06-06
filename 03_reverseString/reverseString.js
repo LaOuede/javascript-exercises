2eab65ef2a3bd3e09c6f98875081a870f87a1f91
Gwenola <gwenola.leroux@gmail.com>
2024-06-06 14:18:29 -0400

    ex01, ex02 : ok - ex03 : WIP

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let res = [];
+    let len = string.length - 1;
 
+    for (len; len >= 0; len--) {
+        res.push(string[len]);
+    }
+
+    return res;
 };
 
 // Do not edit below this line
