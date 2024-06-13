b36e99638484a0eba0be859bb0c792111d81f31b
Gwenola Le Roux <gwenola.leroux@gmail.com>
2024-06-13 16:00:11 -0400

    tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes= function (str) {
+	const rgx = /[a-zA-Z0-9]/;
 
+
+	const cleanedString = str
+		.toLowerCase()
+		.split('')
+		.filter((char) => rgx.test(char))
+		.join('');
+	
+
+	const reversedString = cleanedString
+		.split('')
+		.reverse()
+		.join('');
+
+	return cleanedString === reversedString;
 };
 
 // Do not edit below this line
