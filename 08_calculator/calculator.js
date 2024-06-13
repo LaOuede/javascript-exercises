7f65a644eb4f50e5e112c26e7c7e340bdddde98a
Gwenola <gwenola.leroux@gmail.com>
2024-06-13 13:54:12 -0400

    tests again

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length > 0) {
+    return array.reduce((acc, num) => acc + num, 0);
+  }
+  return 0;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (array.length > 0) {
+    return array.reduce((acc, num) => acc * num, 1);
+  }
+  return 0;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  return Math.pow(num, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let res = 1;
+
+	for (let i = num; i > 0; i--) {
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
